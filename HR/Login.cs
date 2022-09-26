518247a1aca7bbcc31ac74c24c72850efdc3da4c
kien-phan <kienphan1621@gmail.com>
2022-09-26 20:28:33 +0700

    P Begin

---
+++ HR/Login.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR
+{
+    public partial class fLogin : Form
+    {
+        public fLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void click_textbox1(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+            panel3.BackColor = Color.White;
+            panel4.BackColor = SystemColors.Control;
+            textBox2.BackColor = SystemColors.Control;
+        }
+
+        private void click_textbox2(object sender, EventArgs e)
+        {
+            textBox2.BackColor = Color.White;
+            panel4.BackColor = Color.White;
+            panel3.BackColor = SystemColors.Control;
+            textBox1.BackColor = SystemColors.Control;
+        }
+
+        private void pictureBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = false;
+        }
+
+        private void pictureBox3_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox2.UseSystemPasswordChar = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void fLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thực sự muốn thoát chương trình", "Thoát chương trình?", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
+/*
+class Program
+{
+    delegate int MyDelegate(string s);
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+
+        MyDelegate showString = new MyDelegate(ShowString);
+
+        NhapVaShowTen(showString);
+
+        Console.ReadLine();
+    }
+
+    static void NhapVaShowTen(MyDelegate showTen)
+    {
+        Console.WriteLine("Mời nhập tên của bạn:");
+        string ten = Console.ReadLine();
+        showTen(ten);
+    }
+
+    static int ShowString(string stringValue)
+    {
+        Console.WriteLine(stringValue);
+        return 0;
+    }
+}*/
